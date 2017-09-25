ec1702d0930cd68de2274a20c3108c3775a02229
aim21320 <aim21320@gmail.com>
2017-09-25 14:46:52 -0700

    Testing for terry
    tteryr

--- Testing.c
+++ Testing.c
@@ -4,7 +4,8 @@ task main()
 motor(port1) = 127;
 motor(port2) = 127;// testing github
  wait1Msec = 1000;
- //hello Mingyang
- //hello Terry
  motor(port4) = 127;
+ motor(port5) = 127;
+ motor(port6) = 127;
+
 }
